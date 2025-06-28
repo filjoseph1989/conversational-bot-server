85b030aa243bbe79a0bda3265b5c9c0c30d5776d
fil <elmanfil1989@gmail.com>
2025-06-28 11:58:09 +0800

    added a post endpoint

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
+});
+
+app.post('/api/generate', (req, res) => {
+  res.json({ received: req.body });
 });
 
 app.listen(PORT, () => {
